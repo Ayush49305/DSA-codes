64124761201f2a57e01aa25ad79e0214330148c4
Ayush Raj <ayushrajraj12v@gmail.com>
2025-11-19 23:49:05 +0530

    updated code

--- Array problem/15_find_majority_element.cpp
+++ Array problem/15_find_majority_element.cpp
@@ -1,6 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int majorityElement(vector<int> &arr){
+    for(int i=0;i<arr.size();i++){
+        int cnt=0;
+        for(int j=0;j<arr.size();j++){
+            if(arr[j]==arr[i]){
+                cnt++;
+            }
+        }
+
+        if( cnt>arr.size()/2){
+        return arr[i];
+     }
+        
+    }
+    
+    return -1;
+
+}
+
+
 int main() {
+  int n;
+  cout<<"enter number of element:";
+  cin>>n;
+  vector<int> arr(n);
+
+  cout<<"Enter "<<n<<" Element:";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  int result=majorityElement(arr);
+  if(result!=-1)
+  cout<<"Majority element is: "<< result << endl;
+
+  else
+  cout<<"No majority element found"<<endl;
     return 0;
 }
